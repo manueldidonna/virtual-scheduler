d1811aba9145f94da1c617d06d829d5f9d4445a0
manueldidonna <manueldidonnadev@gmail.com>
2019-01-03 21:31:54 +0100

    Added junit 4.12 and mockito-kotlin 2.1.0 deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,9 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.1.0")
+    // test
+    testImplementation("junit:junit:4.12")
+    testImplementation("com.nhaarman.mockitokotlin2:mockito-kotlin:2.1.0")
 }
 
 tasks.withType<KotlinCompile> {

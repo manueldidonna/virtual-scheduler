3b156ea9ebee8399508e869e9aca7db5192b047a
manueldidonna <manueldidonnadev@gmail.com>
2019-01-10 20:59:42 +0100

    Drastically reduced test time

--- src/test/kotlin/com/manueldidonna/virtualscheduler/VirtualSchedulerTest.kt
+++ src/test/kotlin/com/manueldidonna/virtualscheduler/VirtualSchedulerTest.kt
@@ -28,15 +28,16 @@ class VirtualSchedulerTest {
             // trigger
             vs.schedule(tag = commonTag) {
                 children {
-                    wait(300L) { action() } // 300L - invoked
-                    // 350L % 400L invoked.
-                    // the scheduler is cleared before action is invoked Before 450L is invoked
+                    wait(3L) { action() } // 3L - invoked
+                    //  8L and 13L invoked.
+                    // the scheduler is cleared before action is invoked Before 18L is invoked
+                    // secondSchedule has an higher priority
                     for (i in 0 until 3)
-                        wait(50L) { action() }
+                        wait(5L) { action() }
                 }
                 anonymous { anonAction() }
-            }.schedule(449L, tag = commonTag) {
-                vs.clear() // 449L because virtual scheduler priority system is wonderful
+            }.schedule(18L, tag = "secondSchedule") {
+                vs.clear()
             }.run()
 
             // validation
@@ -53,15 +54,15 @@ class VirtualSchedulerTest {
             val action2: () -> Unit = mock()
 
             // trigger
-            vs.schedule(400L, "firstSchedule") {
+            vs.schedule(50L, "firstSchedule") {
                 alive { action1() }
             }
             // secondSchedule should begin before firstSchedule,
             // but when secondSchedule is added, the first is already captured
             // by the scheduler in the .run() while (next != null) loop
             GlobalScope.launch {
-                delay(100)
-                vs.schedule(100L, "secondSchedule") {
+                delay(30L)
+                vs.schedule(6L, "secondSchedule") {
                     alive { action2() }
                 }
             }
@@ -84,17 +85,17 @@ class VirtualSchedulerTest {
             val tagToDiscard = "scheduleToDiscard"
 
             // trigger
-            vs.schedule(500L, tag = tagToDiscard) {
-                actionToNotInvoke()  // (tagToDiscard, 500) is registered as suspension point
-            }.schedule(700L, tag = tagToDiscard) {
-                actionToInvoke() // (tagToDiscard, 700) is registered as suspension point
-            }.schedule(200L, tag = "discardTag") {
-                // (tagToDiscard, 500) is discarded before the execution of its block
+            vs.schedule(5L, tag = tagToDiscard) {
+                actionToNotInvoke()  // (tagToDiscard, 5) is registered as suspension point
+            }.schedule(7L, tag = tagToDiscard) {
+                actionToInvoke() // (tagToDiscard, 7) is registered as suspension point
+            }.schedule(2L, tag = "discardTag") {
+                // (tagToDiscard, 5) is discarded before the execution of its block
                 vs.discardTag(tagToDiscard)
-            }.schedule(600L, tag = "restoreTag") {
-                // (tagToDiscard, 700) is restored before the execution of its block
+            }.schedule(6L, tag = "restoreTag") {
+                // (tagToDiscard, 7) is restored before the execution of its block
                 vs.restoreTag(tagToDiscard)
-            }.run() // the scheduler will wait anyway for 700 millis.
+            }.run() // the scheduler will wait anyway for 7 millis.
             // Validation checks are executed before and after the time processing, not during the process
 
             // validation
@@ -115,7 +116,7 @@ class VirtualSchedulerTest {
             vs.schedule(tag = tagToDiscard) {
                 children {
                     // "traitor" schedule happen before this 'wait' block and it discards the tag
-                    wait(100L) {
+                    wait(10L) {
                         actionToNotInvoke()
                     }
                 }
@@ -126,7 +127,7 @@ class VirtualSchedulerTest {
                 }
                 // alive invokes its block because of "foreigner" restoration
                 alive { actionToInvoke() }
-            }.schedule(70L, "traitor") {
+            }.schedule(7L, "traitor") {
                 vs.discardTag(tagToDiscard)
             }.run()
 
@@ -144,16 +145,16 @@ class VirtualSchedulerTest {
             val action: () -> Unit = mock()
 
             // trigger
-            vs.schedule(100L, tagToDiscard) {
+            vs.schedule(10L, tagToDiscard) {
                 children {
-                    wait(100L) {
+                    wait(10L) {
                         action()
                     }
                 }
                 anonymous {
                     action()
                 }
-            }.schedule(190L, "traitor") {
+            }.schedule(19L, "traitor") {
                 vs.discardTag(tagToDiscard)
             }.run()
 
@@ -170,10 +171,10 @@ class VirtualSchedulerTest {
             val commonTag = "schedule"
 
             // trigger
-            vs.schedule(500L, tag = commonTag) {
+            vs.schedule(50L, tag = commonTag) {
                 alive { actions[1].invoke() } // 500
                 children {
-                    wait(100L) {
+                    wait(10L) {
                         // wait4 is executed after anonymous3.
                         // each children create a suspension point of 0 millis.
                         // Thanks to this sp, the scheduler jump to wait2
@@ -182,25 +183,25 @@ class VirtualSchedulerTest {
                         // but the order of creation prevails.
                         actions[4].invoke() // 600
                     }
-                    wait(200L) {
+                    wait(20L) {
                         actions[5].invoke() // 800
                     }
                     alive { actions[6].invoke() } // 800
-                    wait(300L) {
+                    wait(30L) {
                         actions[8].invoke() // 1100
                     }
                 }
-            }.schedule(200L, tag = commonTag) {
+            }.schedule(20L, tag = commonTag) {
                 alive { actions[0].invoke() } // 200
                 children {
-                    wait(300L) {
+                    wait(30L) {
                         actions[2].invoke() // 500
                     }
                 }
-                anonymous(100L) {
+                anonymous(10L) {
                     actions[3].invoke() // 600
                 }
-            }.schedule(1100L, commonTag) {
+            }.schedule(110L, commonTag) {
                 alive { actions[7].invoke() }
             }.run()
 
@@ -230,12 +231,12 @@ class VirtualSchedulerTest {
             vs.schedule(tag = tag) {
                 children {
                     while (isAlive()) {
-                        wait(50L) {
+                        wait(5L) {
                             action()
                         }
                     }
                 }
-            }.schedule(201L, tag = "traitor") {
+            }.schedule(21L, tag = "traitor") {
                 vs.discardTag(tag)
             }.run()
 
@@ -280,6 +281,34 @@ class VirtualSchedulerTest {
             }.run()
 
             // validation
+            verify(action, times(1)).invoke()
+        }
+    }
+
+    @Test
+    fun waitWithinAnonymous() {
+        runBlocking {
+            // arrange
+            val action: () -> Unit = mock()
+
+            // trigger
+            vs.schedule(tag = "schedule") {
+                alive {
+                    vs.discardTag(scheduleTag)
+                    anonymous {
+                        // wait parent is anonymous and not schedule
+                        wait(1L) { action() }
+                    }
+                }
+                dead {
+                    anonymous {
+                        // children overrides anonymous tag (that can't be discarded) with discarded scheduleTag
+                        children(scheduleTag) { action() }
+                    }
+                }
+            }.run()
+
+            // trigger
             verify(action, times(1)).invoke()
         }
     }
